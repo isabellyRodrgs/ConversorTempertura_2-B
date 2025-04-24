f1a9ae2f6a9bb080832af2d2c5e433a4e2ebc10a
2024105070063 <2024105070063@aqs-lab-c-06.mshome.net>
2025-04-24 14:42:56 -0400

    Criada a classe temperatura e iniciada a implementação da classe temperaturaEspecial

--- src/main/java/Temperatura.java
+++ src/main/java/Temperatura.java
@@ -1,3 +1,29 @@
+//  Herança quando reaproveitamos atributos em comum para outras classes 
 public class Temperatura {
+    //Atributo privado para armazenar temperatura em celsius
+    private double tempCelsius;
     
+    //Construtor cria objetos com uma temperatura inicial
+    public Temperatura(double tempInicialCelsius){
+        this.tempCelsius = tempInicialCelsius;
+    }
+    
+        //Getter para ler temperatura
+        public double getTemperatura(){
+            return tempCelsius;
+    }
+        //Setter para mudar a temperatura
+        public void setTemperaturaCelsius(double mudarTempCelsius){
+            this.tempCelsius = mudarTempCelsius;
+        }
+        
+        //Método para converter para fahrenheit
+        public double converterParaFahrenheit(){
+            return (tempCelsius*9/5)+32;
+        }
+        
+        //Método para converter de Celsius para Kelvin
+        public double converterParaKelvin(){
+            return tempCelsius+273.15;
+        }
 }
---
+++ src/main/java/TemperaturaEspecial.java
@@ -0,0 +1,8 @@
+public class TemperaturaEspecial extends Temperatura {
+    
+    public TemperaturaEspecial(double tempInicialCelsius) {
+        super(tempInicialCelsius);
+        //"super" pega essa informação e leva para a classe pai no caso "Temperatura"
+    }
+    
+}
